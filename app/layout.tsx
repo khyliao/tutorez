f4f473c64ee603a695528618993f9394ba1980bd
khilyao <sfwew19@gmail.com>
2024-07-24 18:18:20 +0300

    setup seo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,11 +2,44 @@ import type { Metadata } from "next";
 import "modern-normalize";
 import "./globals.css";
 import Navbar from "@/app/components/Navbar";
-import { useLang } from "@hooks/useLang";
 
 export const metadata: Metadata = {
-  title: "Tutorez",
-  description: "Tutorez platform",
+  title: "Tutorez - Платформа онлайн-навчання",
+  description:
+    "Tutorez - це провідна платформа для онлайн-навчання, яка пропонує якісні освітні послуги з різних предметів та допомагає учням досягати успіху в навчанні.",
+  openGraph: {
+    title: "Tutorez - Платформа онлайн-навчання",
+    description:
+      "Tutorez - це провідна платформа для онлайн-навчання, яка пропонує якісні освітні послуги з різних предметів та допомагає учням досягати успіху в навчанні.",
+    images: [
+      {
+        url: "/public/preview.png",
+        width: 800,
+        height: 600,
+        alt: "Tutorez - Платформа онлайн-навчання",
+      },
+    ],
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Tutorez - Платформа онлайн-навчання",
+    description:
+      "Tutorez - це провідна платформа для онлайн-навчання, яка пропонує якісні освітні послуги з різних предметів та допомагає учням досягати успіху в навчанні.",
+    images: [
+      {
+        url: "/public/preview.png",
+        width: 800,
+        height: 600,
+        alt: "Tutorez - Платформа онлайн-навчання",
+      },
+    ],
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+  },
+  generator: "Next.js",
 };
 
 export default function RootLayout({
