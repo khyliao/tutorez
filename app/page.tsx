9dbfc0746e2bd6962ccef7251e4d513f72436382
khilyao <sfwew19@gmail.com>
2025-03-29 16:49:43 +0200

    feat: add auto-open modal on render

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useState, Suspense } from "react";
+import { useState, Suspense, useEffect } from "react";
 import { Analytics } from "@vercel/analytics/react";
 import dynamic from "next/dynamic";
 import Hero from "@components/Hero";
@@ -9,6 +9,7 @@ import "./globals.css";
 import Navbar from "./components/Navbar";
 import QrModal from "./components/QrModal";
 import QrModalWrapper from "./components/QrModal/components/QrModalWrapper";
+import { useRouter } from "next/navigation";
 
 const LazyComponents = {
   Team: dynamic(() => import("@components/Team"), { ssr: false }),
@@ -20,6 +21,12 @@ const LazyComponents = {
 
 const Home = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const router = useRouter();
+
+  // temporary
+  useEffect(() => {
+    router.replace("/?openModal=true");
+  }, []);
 
   return (
     <>
